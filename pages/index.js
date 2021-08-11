b23ea4be829ce3aacbbe8dda281f81b03b732552
kunal patel <pkunal257@gmail.com>
2021-08-11 14:55:37 +0530

    small card

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
-import Head from 'next/head'
-import Banner from '../components/Banner'
-import Header from '../components/Header'
+import Head from "next/head";
+import Banner from "../components/Banner";
+import Header from "../components/Header";
+import SmallCard from "../components/SmallCard";
 
-export default function Home() {
+export default function Home({ exploreData }) {
   return (
     <div className="">
       <Head>
@@ -10,12 +11,38 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-     
-    
-    <Header/>
-    <Banner/>
+      <Header />
+      <Banner />
+      {/* main section  */}
+      <main className="max-w-7xl mx-auto px-8 sm:px-16 ">
+        <section className="pt-6">
+          <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
 
-      
+          {/* pull some from a server */}
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+          {exploreData?.map((item) => (
+            <SmallCard
+              key={item.img}
+              img={item.img}
+              distance={item.distance}
+              location={item.location}
+            />
+          ))}
+          </div>
+         
+        </section>
+      </main>
     </div>
-  )
+  );
+}
+export async function getStaticProps() {
+  const exploreData = await fetch("https://links.papareact.com/pyp").then(
+    (res) => res.json()
+  );
+
+  return {
+    props: {
+      exploreData,
+    },
+  };
 }
