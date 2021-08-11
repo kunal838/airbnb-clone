7c4815009d5331935cf266d1427f3b125134cd0f
kunal patel <pkunal257@gmail.com>
2021-08-11 13:44:53 +0530

    header-searchbar

--- components/Header.js
+++ components/Header.js
@@ -1,16 +1,31 @@
-import Image from 'next/image'
+import Image from "next/image";
+import { SearchIcon } from "@heroicons/react/solid";
+
 function Header() {
   return (
-    <header>
+    <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
       {/* left */}
-      <div>
-          <Image 
-            src ='https://links.papareact.com/qd3'
-            layout='fill'
-          />
+      <div
+        className="relative flex items-center h-10 cursor-pointer
+      my-auto 
+      "
+      >
+        <Image
+          src="https://links.papareact.com/qd3"
+          layout="fill"
+          objectFit="contain"
+          objectPosition="left"
+        />
       </div>
-      {/* middle */}
-      <div></div>
+      {/* middle search*/}
+      <div className="  flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
+        <input
+          className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400"
+          type="text"
+          placeholder="start your search"
+        />
+        <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2" />
+      </div>
       {/* right */}
       <div></div>
     </header>
